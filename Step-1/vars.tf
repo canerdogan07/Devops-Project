d8e37e539573885ccfe7ba771af4aa10e6df481d
Can Erdoğan <thecanerdogan@gmail.com>
2022-05-05 23:04:13 +0300

    Step-1 Commit

--- Step-1/vars.tf
+++ Step-1/vars.tf
@@ -5,15 +5,15 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-central-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-central-1 = "ami-09439f09c55136ecf"
+    us-west-2    = "ami-06b94666"
+    eu-west-1    = "ami-844e0bf7"
   }
 }
 
