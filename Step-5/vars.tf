142eb7b86858a8f9e831f1a8b9e894be377f1d53
Can Erdoğan <thecanerdogan@gmail.com>
2022-05-11 22:27:10 +0300

    Success.

--- Step-5/vars.tf
+++ Step-5/vars.tf
@@ -31,10 +31,10 @@ variable "DUMMY_SSH_PUB_KEY" {
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCySrVgnlDjgO1O0xNj7KLQ8aFh6y3DMEoqpSgvk8pMaG4hqJmYOGLcYr9SNbRThqnalweFfzDQIbNGK6PQcEWKYfxUwogjsn65OOUHdD91MtqiNg5MW3bFk2wlpXs5T83ASqnafmcSbsU3AWFoTpS+4xFYbRUTQVwos85nkuxpVohIwfkGqyZXyPjVZku1OvXLTxI+AjPqPpFTlzTtGT7swklNTd76QSiQU7o4206/93JZKivedqrZAhgstG5jm8EwDeSbJzkm9W22hKT5Or7viyFasQruqYZ12FlzURVw5IvyqmNxr2ncEgSXFCcIFYOaxuQNbW0SeSg++dn0Cezl root@ubuntu-xenial"
 }
 
-variable "AWS_ACCESS_KEY" {
+# variable "AWS_ACCESS_KEY" {
   
-}
+# }
 
-variable "AWS_SECRET_KEY" {
+# variable "AWS_SECRET_KEY" {
   
-}
+# }
