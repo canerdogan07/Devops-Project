125810a1c6cb03305de9d4dd0705f19923b9c250
Can Erdoğan <thecanerdogan@gmail.com>
2022-05-12 15:46:39 +0300

    ip change

--- Step-5/backend.tf
+++ Step-5/backend.tf
@@ -1,7 +1,7 @@
-terraform {
- backend "s3" {
-   bucket = "canerdogan-devops-project"
-   key    = "terraform.tfstate"
-   region = "eu-central-1"
- }
-}
+# terraform {
+#  backend "s3" {
+#    bucket = "canerdogan-devops-project"
+#    key    = "terraform.tfstate"
+#    region = "eu-central-1"
+#  }
+# }
